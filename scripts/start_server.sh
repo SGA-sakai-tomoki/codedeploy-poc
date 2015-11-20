65bad9ca7a384a0f012783f8a9290af9233a9933
Tomoki Sakai <Tomoki.Sakai@jp.sony.com>
2015-11-20 10:35:31 +0900

    add codedeploy scripts

---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo -e "start_server.sh started. `date +'%Y/%m/%d %H:%M:%S'`\n" >> /tmp/ops_start_server.log
---
+++ scripts/stop_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo -e "stop_server.sh started. `date +'%Y/%m/%d %H:%M:%S'`\n" >> /tmp/ops_stop_server.log
